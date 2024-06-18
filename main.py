50b868777b45efadeb08195c63edf73ef992dcac
Peter Soava <thelingo56@gmail.com>
2024-06-17 18:14:05 -0700

    add revised turning logic with new error detection

--- main.py
+++ main.py
@@ -17,16 +17,12 @@ ev3.screen.set_font(small_font)
 
 # Write your program here.
 # Define variables and constants
-# TODO Adjust the tolerance detection so that there's a wide window to go straight
-# However, improve how the robot responds outside of the window
-# Unsure the best way to do this, but try to find a way so that on straights it has a more forgiving Kp
-# But then on corners have a steeper Kp
 tolerance = 10
-blue_tolerance = 4
-green_tolerance = 3
-white_tolerance = 3
-base_speed = 75  # Base speed for the motors
-Kp = 8  # Proportional gain
+blue_tolerance = 20
+green_tolerance = 9
+white_tolerance = 7
+base_speed = 120  # Base speed for the motors
+Kp = 40  # Proportional gain
 Ki = 0.01  # Integral gain
 Kd = 0  # Derivative gain
 integral = 0
@@ -57,7 +53,7 @@ while True:
     # Check if the button is pressed to toggle motors
     if Button.CENTER in ev3.buttons.pressed():
         motors_enabled = not motors_enabled
-        if motors_enabled == False:
+        if not motors_enabled:
             left_motor.stop()
             right_motor.stop()
         wait(500)  # Wait for 0.5 seconds to avoid rapid toggling
@@ -78,14 +74,14 @@ while True:
         
     # Check if the right button is pressed to increase speed
     if Button.UP in ev3.buttons.pressed():
-        Kp += 0.5
-        if Kp > 40:  # You can adjust the maximum speed limit as needed
-            Kp = 40
+        Kp += 1
+        if Kp > 100:  # You can adjust the maximum speed limit as needed
+            Kp = 100
         wait(200)  # Wait for 0.5 seconds to avoid rapid toggling
         
     # Check if the left button is pressed to decrease speed
     if Button.DOWN in ev3.buttons.pressed():
-        Kp -= 0.5
+        Kp -= 1
         if Kp < 1:
             Kp = 1
         wait(200)  # Wait for 0.5 seconds to avoid rapid toggling
@@ -106,7 +102,7 @@ while True:
     ev3.screen.draw_text(0, 30, "Object Distance: {}".format(distance))
 
     # Check if an object is within 10cm of the ultrasonic sensor
-    if distance <= 1 and not object_action_performed:
+    if distance <= 100 and not object_action_performed:
         # Stop the motors
         left_motor.stop()
         right_motor.stop()
@@ -152,8 +148,6 @@ while True:
 
     # Calculate the error term
     error = threshold - current_intensity
-    
-    error = error
 
     # Integral term
     integral += error
@@ -162,9 +156,35 @@ while True:
     derivative = error - prev_error
     prev_error = error
 
-    # Adjust motor speeds proportionally based on the error, integral, and derivative terms
-    left_speed = base_speed + (Kp * error) + (Ki * integral) + (Kd * derivative)
-    right_speed = base_speed - (Kp * error) - (Ki * integral) - (Kd * derivative)
+    # Calculate the difference between error and tolerance
+    error_diff = abs(error) - tolerance
+
+    # Adjust motor speeds based on the error difference
+    if error > tolerance:
+        # Too dark, turn left
+        left_speed = base_speed + (Kp * error_diff) + (Ki * integral) + (Kd * derivative)
+        right_speed = base_speed - (Kp * error_diff) - (Ki * integral) - (Kd * derivative)
+        if motors_enabled:
+            left_motor.run(left_speed)
+            right_motor.run(right_speed)
+        ev3.screen.draw_text(0, 50, "Turn Right")
+        ev3.screen.draw_text(0, 60, "Error({}): Too dark".format(round(error,4)))
+    elif error < -tolerance:
+        # Too bright, turn right
+        left_speed = base_speed - (Kp * error_diff) - (Ki * integral) - (Kd * derivative)
+        right_speed = base_speed + (Kp * error_diff) + (Ki * integral) + (Kd * derivative)
+        if motors_enabled:
+            left_motor.run(left_speed)
+            right_motor.run(right_speed)
+        ev3.screen.draw_text(0, 50, "Turn Left")
+        ev3.screen.draw_text(0, 60, "Error ({}): Too bright".format(round(error,4)))
+    else:
+        # In the desired range, go straight
+        if motors_enabled:
+            left_motor.run(base_speed)
+            right_motor.run(base_speed)
+        ev3.screen.draw_text(0, 50, "Straight")
+        ev3.screen.draw_text(0, 60, "Error ({}): In range".format(round(error,4)))
 
     # Set tolerances based on color
     if green_value < blue_difference:  # if blue
@@ -178,29 +198,6 @@ while True:
     else:  # if white
         tolerance = white_tolerance
         ev3.screen.draw_text(0, 40, "WHITE")
-
-    # Apply tolerance
-    if error > tolerance:
-        # Too dark, turn left
-        if motors_enabled:
-            left_motor.run(left_speed)
-            right_motor.run(right_speed)
-        ev3.screen.draw_text(0, 50, "Turn Right")
-        ev3.screen.draw_text(0, 60, "Error({}): Too dark".format(round(error,4)))
-    elif error < -tolerance:
-        # Too bright, turn right
-        if motors_enabled:
-            left_motor.run(left_speed)
-            right_motor.run(right_speed)
-        ev3.screen.draw_text(0, 50, "Turn Left")
-        ev3.screen.draw_text(0, 60, "Error ({}): Too bright".format(round(error,4)))
-    else:
-        # In the desired range, go straight
-        if motors_enabled:
-            left_motor.run(base_speed)
-            right_motor.run(base_speed)
-        ev3.screen.draw_text(0, 50, "Straight")
-        ev3.screen.draw_text(0, 60, "Error ({}): In range".format(round(error,4)))
 
     # Display info about if motors
     ev3.screen.draw_text(0, 70, "Motors Enabled: {}".format(motors_enabled))
